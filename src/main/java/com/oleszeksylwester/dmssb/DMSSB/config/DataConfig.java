44d53208bc87ac26a86838cefca5180144957b99
Sylwester Oleszek <sylwester@localhost.localdomain>
2019-02-23 21:48:36 +0100

    setting database

--- src/main/java/com/oleszeksylwester/dmssb/DMSSB/config/DataConfig.java
+++ src/main/java/com/oleszeksylwester/dmssb/DMSSB/config/DataConfig.java
@@ -14,12 +14,9 @@ public class DataConfig {
     public DriverManagerDataSource dataSource() {
         DriverManagerDataSource ds = new DriverManagerDataSource();
         ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        /*ds.setUrl("jdbc:mysql://localhost:3306/dmssb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");*/
         ds.setUrl("jdbc:mysql://172.30.150.103:3306/dmssb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
         ds.setUsername("user");
-        /*ds.setUsername("root");*/
         ds.setPassword("password");
-        /*ds.setPassword("Rzeszów1983@");*/
         return ds;
     }
 }
